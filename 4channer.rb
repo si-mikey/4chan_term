0eafaa6a8e907b984852042b35ba38052390738f
Luis <mikeylopez@gmail.com>
2016-07-13 22:01:33 -0400

    page number to string, test link with equals

--- 4channer.rb
+++ 4channer.rb
@@ -8,7 +8,9 @@ base_url = "http://boards.4chan.org/gif/"
 
 (1..10).each do |page_num|
   page_num = "" if page_num == 1
-  document = Oga.parse_html(open(base_url + page_num))
+  document = Oga.parse_html(open(base_url + page_num.to_s))
+  puts "######## Page #{page_num} ##########"
+  puts ""
   document.css(".thread").each_with_index do |thread, index|
     next if index == 0
     puts "###### Thread #{index} ######"
@@ -17,14 +19,14 @@ base_url = "http://boards.4chan.org/gif/"
     message = thread.at_css("blockquote").text
     puts "Summary: " + message
     link = base_url + thread.at_css(".summary .replylink").get('href') rescue NoMethodError
-    puts "Link: " + link unless link.is_a? NoMethodError
+    puts "Link: " + link unless link == NoMethodError
     puts "###### Thread #{index} ######"
     puts "######-----------------######"
     puts ""
     puts "open thread?: [y]es/[n]o/[q]uit"
     y_n = gets
     if y_n.strip == 'y'
-      system("chromium-browser #{link}")
+      system("chromium-browser #{link} > /dev/null")
     elsif y_n.strip == 'q'
       abort
     end
