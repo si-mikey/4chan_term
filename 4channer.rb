b76eb977ab2fddcb02ea9e80c30059aa5b456689
Luis <mikeylopez@gmail.com>
2016-07-17 22:30:14 -0400

    make downcase input

--- 4channer.rb
+++ 4channer.rb
@@ -24,7 +24,7 @@ base_url = "http://boards.4chan.org/gif/"
     puts "######-----------------######"
     puts ""
     puts "open thread?: [y]es/[n]o/[q]uit" if link != NoMethodError
-    y_n = gets
+    y_n = gets.downcase
     y_n = 'n' if link == NoMethodError
     if y_n.strip == 'y'
       system("chromium-browser #{link} > /dev/null")
